8c01645fb91beadef8916e760563af370c122cb3
krssachinn@gmail.com <krssachinn@gmail.com>
2025-01-07 15:37:36 +0530

    add shadcn

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Button} from '@/components/ui/button'
+
+const App = () => {
+  return (
+    <div>
+      <Button>hello</Button>
+    </div>
+  )
+}
+
+export default App
