3c782a35b0db89a8e77a80c45d0e7f6479a9243d
krssachinn@gmail.com <krssachinn@gmail.com>
2025-01-07 18:46:54 +0530

    add bg-auth-img

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import victory from '@/assets/victory.svg'
+import Background from '@/assets/login2.png'
 import { Input } from "@/components/ui/input";
 import { Button } from '@/components/ui/button';
 import {Tabs, TabsContent, TabsList, TabsTrigger} from '@/components/ui/tabs'
@@ -8,63 +9,66 @@ const Auth = () => {
   return (
     <div className="h-screen w-screen flex justify-center items-center">
       <div className="shadow-2xl h-5/6 w-4/5 flex justify-center items-center">
-        <div className="flex flex-col gap-2 ">
+        <div className="flex flex-col md:flex-row md:gap-9 gap-2 ">
           <div>
-            <div className="flex items-center justify-center">
-              <h2 className="text-4xl font-bold">Welcome</h2>
-              <img src={victory} alt="victory" className="h-[100px]" />
+            <div>
+              <div className="flex items-center justify-center">
+                <h2 className="text-4xl font-bold">Welcome</h2>
+                <img src={victory} alt="victory" className="h-[100px]" />
+              </div>
+              <p className="mt-[-25px] text-xs font-semibold text-neutral-500">
+                please Login/Signup first to enjoy our free chat app!
+              </p>
             </div>
-            <p className="mt-[-25px] text-xs font-semibold text-neutral-500">
-              please Login/Signup first to enjoy our free chat app!
-            </p>
-          </div>
 
-          <Tabs defaultValue="login" className="w-full justify-center mt-4">
-            <TabsList className="flex ">
-              <TabsTrigger className="w-full" value="login">
-                Login
-              </TabsTrigger>
-              <TabsTrigger className="w-full" value="signup">
-                Sign Up
-              </TabsTrigger>
-            </TabsList>
-            <TabsContent value="login" className="flex flex-col gap-2 ">
-              <Input
-                className="mt-2 rounded-full outline-none text-xs placeholder:text-neutral-400 active:outline-none"
-                type="email"
-                placeholder="email"
-                required
-              />
-              <Input
-                className="rounded-full outline-none text-xs placeholder:text-neutral-400 active:outline-none"
-                type="password"
-                placeholder="password"
-                required
-              />
-              <Button className="rounded-full mt-2">Login</Button>
-            </TabsContent>
-            <TabsContent value="signup" className="flex flex-col gap-2">
-              <Input
-                className="rounded-full outline-none text-xs placeholder:text-neutral-400 active:outline-none"
-                type="email"
-                placeholder="email"
-                required
-              />
-              <Input
-                className="rounded-full outline-none text-xs placeholder:text-neutral-400 active:outline-none"
-                type="password"
-                placeholder="password"
-                required
-              />
-              <Input
-                className="rounded-full outline-none text-xs placeholder:text-neutral-400 active:outline-none"
-                type="password"
-                placeholder="confirm password"
-                required
-              />
-              <Button className="rounded-full mt-2">Login</Button>
-            </TabsContent>
-          </Tabs>
+            <Tabs defaultValue="login" className="w-full justify-center mt-4">
+              <TabsList className="flex ">
+                <TabsTrigger className="w-full" value="login">
+                  Login
+                </TabsTrigger>
+                <TabsTrigger className="w-full" value="signup">
+                  Sign Up
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent value="login" className="flex flex-col gap-2 ">
+                <Input
+                  className="mt-2 rounded-full outline-none text-xs placeholder:text-neutral-400 active:outline-none"
+                  type="email"
+                  placeholder="email"
+                  required
+                />
+                <Input
+                  className="rounded-full outline-none text-xs placeholder:text-neutral-400 active:outline-none"
+                  type="password"
+                  placeholder="password"
+                  required
+                />
+                <Button className="rounded-full mt-2">Login</Button>
+              </TabsContent>
+              <TabsContent value="signup" className="flex flex-col gap-2">
+                <Input
+                  className="rounded-full outline-none text-xs placeholder:text-neutral-400 active:outline-none"
+                  type="email"
+                  placeholder="email"
+                  required
+                />
+                <Input
+                  className="rounded-full outline-none text-xs placeholder:text-neutral-400 active:outline-none"
+                  type="password"
+                  placeholder="password"
+                  required
+                />
+                <Input
+                  className="rounded-full outline-none text-xs placeholder:text-neutral-400 active:outline-none"
+                  type="password"
+                  placeholder="confirm password"
+                  required
+                />
+                <Button className="rounded-full mt-2">Login</Button>
+              </TabsContent>
+            </Tabs>
+          </div>
+          <img src={Background} alt="" className='h-[60vh] hidden md:block' />
         </div>
       </div>
     </div>
