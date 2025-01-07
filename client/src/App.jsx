ae11bd02f956007d2cddfe62e034f778fa503dd6
krssachinn@gmail.com <krssachinn@gmail.com>
2025-01-07 16:46:08 +0530

    add react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import {Button} from '@/components/ui/button'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Auth from './pages/auth'
+import Chat from './pages/chat'
+import Profile from './pages/profile'
 
 const App = () => {
   return (
-    <div>
-      <Button>hello</Button>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='*' element={<Navigate to={'/auth'}/>}/>
+      <Route path='/auth' element={<Auth/>}/>
+      <Route path='/chat' element={<Chat/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
