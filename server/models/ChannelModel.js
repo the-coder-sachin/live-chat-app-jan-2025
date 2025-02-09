f74551b9037c0ce7fe915d6099e4290d59ffa90a
krssachinn@gmail.com <krssachinn@gmail.com>
2025-02-09 23:12:18 +0530

    add channel model

---
+++ server/models/ChannelModel.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const channelSchema = new mongoose.Schema({
+    name: {type: String, required:true},
+    members: [{type:mongoose.Schema.Types.ObjectId, ref:'users', required:true}],
+    message: [{type:mongoose.Schema.Types.ObjectId, ref:'messages', required:false}],
+    admin: {type:mongoose.Schema.Types.ObjectId, ref:'users', required:true},
+    createdAt: {type:Date, default:Date.now()},
+    updatedAt: {type:Date, default:Date.now()},
+})
+
+channelSchema.pre('save', function(next){
+    this.updatedAt = Date.now();
+    next()
+});
+
+channelSchema.pre('findOneAndUpdate', function(next){
+    this.set({updatedAt : Date.now()})
+    next()
+});
+
+const channelModel = mongoose.model('channels', channelSchema)
