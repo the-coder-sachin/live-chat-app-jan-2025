62e2ade6a721bcf61bf5edd8224ae9fbf5a46214
krssachinn@gmail.com <krssachinn@gmail.com>
2025-10-06 16:15:20 +0530

    Update API route constants to include host URL for consistency

--- client/utils/constants.js
+++ client/utils/constants.js
@@ -1,7 +1,7 @@
 
 export const host = import.meta.env.VITE_SERVER_URL
 
-export const AUTH_ROUTES = `api/auth`
+export const AUTH_ROUTES = `${host}/api/auth`
 export const SIGNUP_ROUTE = `${AUTH_ROUTES}/signup`
 export const LOGIN_ROUTE = `${AUTH_ROUTES}/login`
 export const USER_INFO_ROUTE = `${AUTH_ROUTES}/user-info`
@@ -11,17 +11,17 @@ export const REMOVE_PROFILE_IMAGE_ROUTE = `${AUTH_ROUTES}/remove-profile-image`
 export const LOGOUT_ROUTE = `${AUTH_ROUTES}/logout`
 
 
-export const CONTACT_ROUTES = `api/contact`
+export const CONTACT_ROUTES = `${host}/api/contact`
 export const SEARCH_CONTACT = `${CONTACT_ROUTES}/search-contact`
 export const GET_DM_LIST = `${CONTACT_ROUTES}/get-contact-dm-list`;
 export const GET_ALL_CONTACTS = `${CONTACT_ROUTES}/get-all-contacts`
 
 
-export const MESSAGES_ROUTES = `api/messages`
+export const MESSAGES_ROUTES = `${host}/api/messages`
 export const GET_ALL_MESSAGES = `${MESSAGES_ROUTES}/get-messages`
 export const UPLOAD_FILES = `${MESSAGES_ROUTES}/upload-file`
 
-export const CHANNEL_ROUTES = `api/channel`
+export const CHANNEL_ROUTES = `${host}/api/channel`
 export const CREATE_CHANNEL = `${CHANNEL_ROUTES}/create-channel`
 export const GET_USER_CHANNEL = `${CHANNEL_ROUTES}/get-user-channel`
 export const GET_CHANNEL_MESSAGES = `${CHANNEL_ROUTES}/get-channel-messages`;
